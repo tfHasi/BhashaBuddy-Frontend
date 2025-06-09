808fc74f6ee1adf448e96d09a696ddbe5edb97b5
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-06-10 01:53:18 +0530

    CREATE: score widget

---
+++ frontend/lib/screens/widgets/score.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import '../../services/websocket_service.dart';
+
+class ScoreWidget extends StatefulWidget {
+  final String userId;
+  final String nickname;
+  final int initialStars;
+
+  const ScoreWidget({
+    Key? key,
+    required this.userId,
+    required this.nickname,
+    this.initialStars = 0,
+  }) : super(key: key);
+
+  @override
+  State<ScoreWidget> createState() => _ScoreWidgetState();
+}
+
+class _ScoreWidgetState extends State<ScoreWidget> {
+  late WebSocketService _webSocketService;
+  StreamSubscription<Map<String, dynamic>>? _scoreSubscription;
+  int _totalStars = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _totalStars = widget.initialStars;
+    _webSocketService = WebSocketService();
+    _connectToWebSocket();
+  }
+
+  void _connectToWebSocket() async {
+    try {
+      await _webSocketService.connectToScoreUpdates();
+      _scoreSubscription = _webSocketService.scoreUpdates.listen((data) {
+        final userId = data['user_id']?.toString();
+        if (userId == widget.userId) {
+          setState(() {
+            _totalStars = data['total_stars'] ?? _totalStars;
+          });
+        }
+      });
+    } catch (e) {
+      print('Failed to connect to WebSocket: $e');
+    }
+  }
+
+  @override
+  void dispose() {
+    _scoreSubscription?.cancel();
+    _webSocketService.dispose();
+    super.dispose();
+  }
+
+@override
+Widget build(BuildContext context) {
+  return Container(
+    width: 60,
+    height: 30,
+    decoration: BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage('assets/images/score_input_rectangle.png'),
+        fit: BoxFit.fill,
+      ),
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'assets/images/yellow_star.png',
+          width: 22,
+          height: 22,
+        ),
+        SizedBox(width: 8),
+        Text(
+          '$_totalStars',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: const Color.fromARGB(255, 218, 33, 33),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+}
